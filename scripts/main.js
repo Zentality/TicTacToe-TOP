c26e4ee821a92c1d5df8c77e0ac4ccaa2901ccd1
Zentality <jmcp@live.com.au>
2022-05-10 17:02:19 +0800

    Add player symbol to DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,22 @@
 const gameBoard = (() => {
   const board = ["","","","","","","","",""];
-  return {board};
+  const play = (index, symbol) => {
+    board[index] = symbol;
+  }
+  return {play};
 })();
 
 const displayController = (() => {
   const boardSquares = document.querySelectorAll(".main>ul>li");
+
   boardSquares.forEach((square, index) => {
-    square.addEventListener("click", (e) => {
-      gameBoard.board[index] = "X";
-      console.log(gameBoard.board);
+    square.addEventListener("click", () => {
+      gameBoard.play(index, "X");
+      square.textContent = "X";
     });
   });
+
+  
 })();
 
 const Player = (symbol) => {
